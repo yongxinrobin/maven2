aa58273b540d04ae9747d4de95f810804ee691b5
justlike44 <1101001900@qq.com>
2017-08-24 00:11:26 +0800

    update code
    Signed-off-by: justlike44 <1101001900@qq.com>

---
+++ src/main/java/testMain/Test1.java
@@ -0,0 +1,39 @@
+package testMain;
+
+/**
+ * 这个小程序说明流程, 当调用一个类的方法时, 先是执行代码块的, 
+ * 接着是构造方法里的, 再接着是方法的
+ * @author wubinbin
+ */
+public class Test1 {
+    public Test1() {
+    	System.out.println("构造方法 ************   ");
+	}
+    public void show(){
+    	System.out.println("声明方法 ############# ");
+    }
+    // 代码块
+    {   
+        System.out.println("代码 块 %%%%%%%%%%%%%%%% ");
+    }
+    public static void main(String[] args) {
+    	System.out.println("main 方法 $$$$$$$$$$ ");
+		Test1 test = new Test1();
+		test.show();
+	}
+    // static block 的优先顺序比 main static method 高, 先执行这个代码块的
+    static{
+    	System.out.println(" 静态 代码块 static block---------- ");
+    }
+    
+/**
+ * 
+输出流程如下：
+静态 代码块 static block---------- 
+main 方法 $$$$$$$$$$ 
+代码 块 %%%%%%%%%%%%%%%% 
+构造方法 ************   
+声明方法 ############# 
+*/
+    
+}
